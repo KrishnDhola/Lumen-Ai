e5cfce2537a0fd808c270ffc3d9c9cb14bcaccff
KrishnDhola <159628401+KrishnDhola@users.noreply.github.com>
2025-07-18 08:20:27 +0000

    updated package.json

--- lumen-ai/vite.config.ts
+++ lumen-ai/vite.config.ts
@@ -1,8 +1,10 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
